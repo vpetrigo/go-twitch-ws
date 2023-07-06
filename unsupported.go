4bda195b6d1836aea63c2751ba4914eba399d25a
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-07-06 23:44:16 +0300

    unsupported: Add list of currently unsupported EventSub events

---
+++ unsupported.go
@@ -0,0 +1,55 @@
+package twitchws
+
+// type ChannelUpdateEvent struct{}
+// type ChannelFollowEvent struct{}
+
+type ChannelSubscribeEvent struct{}
+type ChannelSubscriptionEndEvent struct{}
+type ChannelSubscriptionGiftEvent struct{}
+type ChannelSubscriptionMessageEvent struct{}
+type ChannelCheerEvent struct{}
+type ChannelRaidEvent struct{}
+type ChannelBanEvent struct{}
+type ChannelUnbanEvent struct{}
+type ChannelModeratorAddEvent struct{}
+type ChannelModeratorRemoveEvent struct{}
+type ChannelGuestStarSessionBeginEvent struct{}
+type ChannelGuestStarSessionEndEvent struct{}
+type ChannelGuestStarGuestUpdateEvent struct{}
+type ChannelGuestStarSlotUpdateEvent struct{}
+type ChannelGuestStarSettingsUpdateEvent struct{}
+type ChannelChannelPointsCustomRewardAddEvent struct{}
+type ChannelChannelPointsCustomRewardUpdateEvent struct{}
+type ChannelChannelPointsCustomRewardRemoveEvent struct{}
+type ChannelChannelPointsCustomRewardRedemptionAddEvent struct{}
+type ChannelChannelPointsCustomRewardRedemptionUpdateEvent struct{}
+type ChannelPollBeginEvent struct{}
+type ChannelPollProgressEvent struct{}
+type ChannelPollEndEvent struct{}
+type ChannelPredictionBeginEvent struct{}
+type ChannelPredictionProgressEvent struct{}
+type ChannelPredictionLockEvent struct{}
+type ChannelPredictionEndEvent struct{}
+type ChannelCharityCampaignDonateEvent struct{}
+type ChannelCharityCampaignStartEvent struct{}
+type ChannelCharityCampaignProgressEvent struct{}
+type ChannelCharityCampaignStopEvent struct{}
+type DropEntitlementGrantEvent struct{}
+type ExtensionBitsTransactionCreateEvent struct{}
+type ChannelGoalBeginEvent struct{}
+type ChannelGoalProgressEvent struct{}
+type ChannelGoalEndEvent struct{}
+type ChannelHypeTrainBeginEvent struct{}
+type ChannelHypeTrainProgressEvent struct{}
+type ChannelHypeTrainEndEvent struct{}
+type ChannelShieldModeBeginEvent struct{}
+type ChannelShieldModeEndEvent struct{}
+type ChannelShoutoutCreateEvent struct{}
+type ChannelShoutoutReceiveEvent struct{}
+
+// type StreamOnlineEvent struct{}
+// type StreamOfflineEvent struct{}
+
+type UserAuthorizationGrantEvent struct{}
+type UserAuthorizationRevokeEvent struct{}
+type UserUpdateEvent struct{}
