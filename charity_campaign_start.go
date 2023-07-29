5b080982a15127d2bca4da7117e1c529cecb7c09
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-07-30 01:32:05 +0600

    eventsub-events: Regenerate Charity related events with intermediate Object support

--- charity_campaign_start.go
+++ charity_campaign_start.go
@@ -10,13 +10,7 @@ type CharityCampaignStartEvent struct {
 	CharityLogo        string      `json:"charity_logo"`        // A URL to an image of the charity’s logo.
 	CharityWebsite     string      `json:"charity_website"`     // A URL to the charity’s website.
 	CurrentAmount      interface{} `json:"current_amount"`      // An object that contains the current amount of donations that the campaign has received.
-	Value              int         `json:"value"`               // The monetary amount.
-	DecimalPlaces      int         `json:"decimal_places"`      // The number of decimal places used by the currency.
-	Currency           string      `json:"currency"`            // The ISO-4217 three-letter currency code that identifies the type of currency in value.
 	TargetAmount       interface{} `json:"target_amount"`       // An object that contains the campaign’s target fundraising goal.
-	Value              int         `json:"value"`               // The monetary amount.
-	DecimalPlaces      int         `json:"decimal_places"`      // The number of decimal places used by the currency.
-	Currency           string      `json:"currency"`            // The ISO-4217 three-letter currency code that identifies the type of currency in value.
 	StartedAt          string      `json:"started_at"`          // The UTC timestamp (in RFC3339 format) of when the broadcaster started the campaign.
 }
 
